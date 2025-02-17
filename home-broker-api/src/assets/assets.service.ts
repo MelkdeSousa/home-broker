4cda177174d9a3fe9a6e732b6f5bbdf09aa9f29a
Melk de Sousa <melk.sousap@gmail.com>
2025-02-17 15:45:01 -0300

    add assets module with controller, service, DTO, and entity definitions

---
+++ home-broker-api/src/assets/assets.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import type { Model } from 'mongoose';
+import { CreateAssetDto } from './dto/create-asset.dto';
+import { Asset } from './entities/asset.entity';
+
+@Injectable()
+export class AssetsService {
+  constructor(
+    @InjectModel(Asset.name)
+    private readonly assetModel: Model<Asset>,
+  ) {}
+
+  create(createAssetDto: CreateAssetDto) {
+    return this.assetModel.create(createAssetDto);
+  }
+
+  findAll() {
+    return this.assetModel.find();
+  }
+
+  findOne(symbol: string) {
+    return this.assetModel.findOne({ symbol });
+  }
+}
---
+++ home-broker-api/src/assets/entities/asset.entity.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import type { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Asset {
+  @Prop({ unique: true, index: true })
+  name: string;
+
+  @Prop({ unique: true, index: true })
+  symbol: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  price: number;
+
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export type AssetDocument = HydratedDocument<Asset>;
+
+export const AssetSchema = SchemaFactory.createForClass(Asset);
