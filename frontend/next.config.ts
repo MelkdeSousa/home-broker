76191ce3ec44bbdbad8fcffa35b405fcf0785a1a
Melk de Sousa <melk.sousap@gmail.com>
2025-02-19 23:29:07 -0300

    feat: add external domain to image configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost", "www.google.com"],
+    domains: ["localhost", "www.google.com", "external-content.duckduckgo.com"],
   },
 };
 
